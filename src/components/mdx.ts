d693725ceba2c1863d2356ae6600b015ac643605
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-09-19 17:28:19 -0400

    refactor(mdx): centralize shared mdx component mappings

---
+++ src/components/mdx.ts
@@ -0,0 +1,13 @@
+import Figure from "./Figure.astro";
+import NoIndent from "./NoIndent.astro";
+import Right from "./Right.astro";
+import Center from "./Center.astro";
+import VerticalPoem from "./VerticalPoem.astro";
+
+export const mdxComponents = {
+  Figure,
+  NoIndent,
+  Right,
+  Center,
+  VerticalPoem,
+};
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,9 @@
 interface ImportMetaEnv {
   readonly PUBLIC_KEYSTATIC_GITHUB_REPO?: string;
 }
+
+declare module "*.astro" {
+  type AstroComponent = (props: Record<string, unknown>) => unknown;
+  const component: AstroComponent;
+  export default component;
+}
